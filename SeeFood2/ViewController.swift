95685b5c4c7499875152b0a97f410b6140afa9cf
Jason Becker <jedbeck@gmail.com>
2019-05-19 19:17:15 -0400

    Initial Commit with connection to IDM Watson

--- SeeFood2/ViewController.swift
+++ SeeFood2/ViewController.swift
@@ -7,14 +7,66 @@
 //
 
 import UIKit
+import VisualRecognition
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let apiKey = "YOURAPIKEY"
+    let version = "2019-05-19"
+    
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+            let visualRecognition = VisualRecognition(version: version, apiKey: apiKey)
+            
+            let imageData = image.jpegData(compressionQuality: 0.01)
+            
+            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            
+            let fileURL = documentsURL.appendingPathComponent("tempImage.jpg")
+            
+            try? imageData?.write(to: fileURL, options: [])
+            
+            visualRecognition.classify(imagesFile: fileURL) { (classifiedImages) in
+                print(classifiedImages)
+            }
+            
+            
+        }
+        else {
+            print("there was an error picking the image")
+        }
+        
     }
 
 
+    @IBAction func camerButtonPressed(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+        
+    }
 }
 
