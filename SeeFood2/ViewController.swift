2945b3771c7c963be11398943f49a68a2f565a2a
Jason Becker <jbecker@newsbank.com>
2019-05-20 13:44:02 -0400

    Completed IBM Watson integrated app with UI Improvements

--- SeeFood2/ViewController.swift
+++ SeeFood2/ViewController.swift
@@ -8,28 +8,39 @@
 
 import UIKit
 import VisualRecognition
+import SVProgressHUD
+import Social
 
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var topBarImageView: UIImageView!
+    @IBOutlet weak var shareButton: UIButton!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     @IBOutlet weak var imageView: UIImageView!
     
-    let apiKey = "YOURAPIKEY"
+    let apiKey = "YOUR APP KEY"
     let version = "2019-05-19"
     
     
     let imagePicker = UIImagePickerController()
+    var classificationResults : [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         imagePicker.delegate = self
+        shareButton.isHidden = true
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        cameraButton.isEnabled = false
+        
+        SVProgressHUD.show()
+        
         
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = image
@@ -47,7 +58,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             try? imageData?.write(to: fileURL, options: [])
             
             visualRecognition.classify(imagesFile: fileURL) { (classifiedImages) in
-                print(classifiedImages)
+                let classes = classifiedImages.images.first!.classifiers.first!.classes
+                
+                self.classificationResults = []
+                
+                for index in 0..<classes.count {
+                    self.classificationResults.append(classes[index].className)
+                }
+                print(self.classificationResults)
+                
+                DispatchQueue.main.async {
+                    self.cameraButton.isEnabled = true
+                    SVProgressHUD.dismiss()
+                    self.shareButton.isHidden = false
+                }
+                
+                if self.classificationResults.contains("hotdog"){
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "Hotdog!"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.green
+                        self.navigationController?.navigationBar.isTranslucent = false
+                        self.topBarImageView.image = UIImage(named: "hotdog")
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "Not Hotdog!"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.red
+                        self.navigationController?.navigationBar.isTranslucent = false
+                        self.topBarImageView.image = UIImage(named: "not-hotdog")
+                    }
+                }
             }
             
             
@@ -66,6 +107,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         present(imagePicker, animated: true, completion: nil)
         
+        
+    }
+    
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        
+        if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter) {
+            let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            vc?.setInitialText("My food is \(navigationItem.title)")
+            vc?.add(UIImage(named: "hotdogBackground"))
+            present(vc!, animated: true, completion: nil)
+            
+        }
+        else {
+            self.navigationItem.title = "Please Login to Twitter"
+        }
         
     }
 }
